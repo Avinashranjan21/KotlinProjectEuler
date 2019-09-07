35c6e6bf1ef59bdc8d6d80622503d6c02a20363b
Avinash <Avinash.ranjan21@gmail.com>
2019-09-07 10:01:11 +0530

    Problem 1 solved.

--- src/Problem_1_To_10.kt
+++ src/Problem_1_To_10.kt
@@ -1,5 +1,22 @@
+fun main() {
+    println("Hello Project Euler")
+//    Problem 1 call
+    multiplesOf3And5()
+
+}
 
 
-fun main(){
-    println("Hello Kotlin")
+/**
+ * Problem 1: If we list all the natural numbers below 10 that are multiples of 3 or 5,
+ * we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000. Answer : 233168
+ * */
+
+fun multiplesOf3And5() {
+    val maxNumber = 1000
+    var result = 0
+    for (i in 0 until maxNumber) {
+        if (i % 3 == 0 || i % 5 == 0) result += i
+    }
+    println("Sum of all the multiples of 3 or 5 below $maxNumber is $result")
 }
